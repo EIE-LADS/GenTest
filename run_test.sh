36cc1676e90d1756cda2a4cdca94e2114b67fb3f
DiederikVink <dav114@ic.ac.uk>
2016-03-15 02:49:02 +0000

    run_test.sh update

--- run_test.sh
+++ run_test.sh
@@ -11,13 +11,13 @@ done
 
 #Run through gcc
 
-mips-linux-gnu-gcc test/driver.c test/test_${NUM}.c -o bin/test_gcc
+mips-linux-gnu-gcc test/driver.c test/${NAME}_test_${NUM}.c -o bin/test_gcc
 bin/test_gcc $X $Y
 ANSW_GCC="$(echo $?)"
 
 #Run my assembly
 mips-linux-gnu -c -S -static test/driver.c -o test/driver.s
-mips_linux-gcc -static test/test_${NUM}.s test/driver.s -o bin/test_mips
+mips_linux-gcc -static test/${NAME}_test_${NUM}.s test/driver.s -o bin/test_mips
 qemu-mips bin/test_mips $X $Y
 ANSW_MIPS="$(echo $?)"
 
