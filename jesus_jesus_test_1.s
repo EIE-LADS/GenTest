f8dd3742481d01c7229d130e5146270020d1b060
jeg114 <jeg114@ic.ac.uk>
2016-03-15 16:34:28 +0000

    saved s

--- jesus_jesus_test_1.s
+++ jesus_jesus_test_1.s
@@ -1,9 +1,13 @@
 	.text
 	.global f
 	.ent f
-	.type f, @function
+	.type f, @func
 f:
 	move $fp, $sp
+	move $t0, $a0
+	sw $t0, 1($fp)
+	move $t0, $a1
+	sw $t0, 2($fp)
 SCOPE_1:
 	li $t0, 1
 	move $v0, $t0
