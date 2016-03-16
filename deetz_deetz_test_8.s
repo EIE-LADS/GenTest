092e1ade4ffce83becdeab6ea47ea4706a386df0
DiederikVink <dav114@ic.ac.uk>
2016-03-16 18:44:59 +0000

    eie

--- deetz_deetz_test_8.s
+++ deetz_deetz_test_8.s
@@ -4,27 +4,27 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-156
-	sw	$31,152($sp)
-	sw	$fp,148($sp)
+	addiu	$sp,$sp,-52
+	sw	$31,48($sp)
+	sw	$fp,44($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
-	lw	$8,128($fp)
-	sw	$8,128($fp)
-	lw	$8,140($fp)
+	lw	$8,28($fp)
+	sw	$8,28($fp)
+	lw	$8,36($fp)
 	li	$8,4
-	sw	$8,140($fp)
-	lw	$8,140($fp)
-	sw	$8,128($fp)
-	sw	$8,136($fp)
-	lw	$8,128($fp)
-	sw	$8,128($fp)
-	lw	$2,128($fp)
+	sw	$8,36($fp)
+	lw	$8,36($fp)
+	sw	$8,28($fp)
+	sw	$8,32($fp)
+	lw	$8,28($fp)
+	sw	$8,28($fp)
+	lw	$2,28($fp)
 	move	$sp,$fp
-	lw	$fp,148($sp)
-	lw	$31,152($sp)
-	addiu	$sp,$sp,156
+	lw	$fp,44($sp)
+	lw	$31,48($sp)
+	addiu	$sp,$sp,52
 	j	$31
 	nop
 
--- deetz_test_8.c
+++ deetz_test_8.c
@@ -1,6 +1,6 @@
 int f (int y, int z)
 {
-	int x, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+	int a;
 	a = 4;
 	return a;
 }
