65703411e69041342224b80cb7c64e0dcb5699f6
DiederikVink <dav114@ic.ac.uk>
2016-03-16 19:31:15 +0000

    updating

---
+++ deetz_deetz_test_24.s
@@ -0,0 +1,45 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-52
+	sw	$31,48($sp)
+	sw	$fp,44($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,16($fp)
+	sw	$8,16($fp)
+	lw	$8,20($fp)
+	sw	$8,20($fp)
+	lw	$8,20($fp)
+	sw	$8,32($fp)
+	addi	$8,$8,1
+	sw	$8,20($fp)
+	lw	$8,0($fp)
+	sw	$8,16($fp)
+	sw	$8,28($fp)
+	lw	$8,16($fp)
+	sw	$8,16($fp)
+	lw	$8,20($fp)
+	sw	$8,20($fp)
+	lw	$8,16($fp)
+	lw	$9,20($fp)
+	mul	$8,$8,$9
+	nop
+	nop
+	sw	$8,36($fp)
+	lw	$2,36($fp)
+	move	$sp,$fp
+	lw	$fp,44($sp)
+	lw	$31,48($sp)
+	addiu	$sp,$sp,52
+	j	$31
+	nop
+
+	.end	f
+	.size	f, .-f
+
+
