4a014232b27ebba5a5e4359bed9792c1aef8d393
DiederikVink <dav114@ic.ac.uk>
2016-03-16 17:19:31 +0000

    working on

--- deetz_adi_test_6.s
+++ deetz_adi_test_6.s
@@ -10,17 +10,18 @@ f:
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
-	lw	$8,36($fp)
-	li	$8,35
-	sw	$8,36($fp)
+	lw	$8,28($fp)
+	sw	$8,28($fp)
+	lw	$8,16($fp)
+	sw	$8,16($fp)
 	lw	$8,40($fp)
 	li	$8,1
 	sw	$8,40($fp)
 	lw	$8,40($fp)
-	lw	$9,36($fp)
+	lw	$9,16($fp)
 	or	$8,$8,$9
-	sw	$8,32($fp)
-	lw	$8,32($fp)
+	sw	$8,36($fp)
+	lw	$8,36($fp)
 	sw	$8,28($fp)
 	lw	$8,28($fp)
 	sw	$8,28($fp)
---
+++ deetz_adi_test_7.s
@@ -0,0 +1,46 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-64
+	sw	$31,60($sp)
+	sw	$fp,56($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,5
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,28($fp)
+	lw	$8,40($fp)
+	li	$8,3
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	sw	$8,36($fp)
+	lw	$8,20($fp)
+	sw	$8,20($fp)
+	lw	$8,36($fp)
+	sw	$8,36($fp)
+	lw	$8,28($fp)
+	sw	$8,28($fp)
+	lw	$8,28($fp)
+	sw	$8,36($fp)
+	lw	$8,48($fp)
+	sw	$8,20($fp)
+	lw	$8,20($fp)
+	sw	$8,20($fp)
+	lw	$2,20($fp)
+	move	$sp,$fp
+	lw	$fp,56($sp)
+	lw	$31,60($sp)
+	addiu	$sp,$sp,64
+	j	$31
+	nop
+
+	.end	f
+	.size	f, .-f
+
+
