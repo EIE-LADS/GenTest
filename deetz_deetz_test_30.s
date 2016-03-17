fd9f09982f8c1e19eb4cbf257e45e80b20e0c89f
DiederikVink <dav114@ic.ac.uk>
2016-03-17 17:36:34 +0000

    updating

---
+++ deetz_deetz_test_30.s
@@ -0,0 +1,66 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-84
+	sw	$31,80($sp)
+	sw	$fp,76($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,1
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,28($fp)
+	lw	$8,40($fp)
+	li	$8,4
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	sw	$8,36($fp)
+	lw	$8,32($fp)
+	li	$8,1
+	sw	$8,32($fp)
+	lw	$8,60($fp)
+	li	$8,3
+	sw	$8,60($fp)
+	lw	$8,60($fp)
+	lw	$9,36($fp)
+	mul	$8,$8,$9
+	nop
+	nop
+	sw	$8,56($fp)
+	lw	$8,68($fp)
+	li	$8,2
+	sw	$8,68($fp)
+	lw	$8,28($fp)
+	lw	$9,68($fp)
+	div	$8,$8,$9
+	nop
+	nop
+	sw	$8,64($fp)
+	lw	$8,64($fp)
+	lw	$9,56($fp)
+	add	$8,$8,$9
+	sw	$8,52($fp)
+	lw	$8,52($fp)
+	lw	$9,32($fp)
+	sub	$8,$8,$9
+	sw	$8,48($fp)
+	lw	$8,48($fp)
+	sw	$8,16($fp)
+	sw	$8,44($fp)
+	lw	$2,16($fp)
+	move	$sp,$fp
+	lw	$fp,76($sp)
+	lw	$31,80($sp)
+	addiu	$sp,$sp,84
+	j	$31
+	nop
+
+	.end	f
+	.size	f, .-f
+
+
---
+++ deetz_test_30.c
@@ -0,0 +1,9 @@
+int f(int a, int b)
+{
+	int x=4; 
+	int y=1;
+	{
+		a = (y/2+3*x-1); 
+	}
+	return a; 
+}
