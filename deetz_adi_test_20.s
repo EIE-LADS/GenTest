10def09151a7689b735e17ac3a059e4264f27afd
DiederikVink <dav114@ic.ac.uk>
2016-03-17 00:29:56 +0000

    updating

---
+++ deetz_adi_test_20.s
@@ -0,0 +1,41 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-56
+	sw	$31,52($sp)
+	sw	$fp,48($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,3
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,28($fp)
+	lw	$8,16($fp)
+	sw	$8,16($fp)
+	lw	$8,0($fp)
+	movz	$8,$0,$8
+	li	$8,1
+	movn	$8,$9,$8
+	sw	840($fp)
+	lw	$8,0($fp)
+	sw	$8,16($fp)
+	sw	$8,36($fp)
+	lw	$8,16($fp)
+	sw	$8,16($fp)
+	lw	$2,16($fp)
+	move	$sp,$fp
+	lw	$fp,48($sp)
+	lw	$31,52($sp)
+	addiu	$sp,$sp,56
+	j	$31
+	nop
+
+	.end	f
+	.size	f, .-f
+
+
