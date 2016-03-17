eea39b64d190b06ca1aff8d8665fbb2a4c7f4470
DiederikVink <dav114@ic.ac.uk>
2016-03-17 16:22:41 +0000

    updtaing

--- deetz_deetz_test_28.s
+++ deetz_deetz_test_28.s
@@ -10,33 +10,33 @@ f:
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
-	lw	$8,40($fp)
-	sw	$8,44($fp)
-	addi	$8,$8,1
-	sw	$8,40($fp)
-	lw	$8,48($fp)
-	li	$8,12
+	lw	$8,28($fp)
 	sw	$8,48($fp)
-	lw	$8,44($fp)
-	lw	$9,48($fp)
+	addi	$8,$8,1
+	sw	$8,28($fp)
+	lw	$8,52($fp)
+	li	$8,12
+	sw	$8,52($fp)
+	lw	$8,48($fp)
+	lw	$9,52($fp)
 	div	$8,$8,$9
 	nop
 	nop
-	sw	$8,36($fp)
-	lw	$8,56($fp)
-	li	$8,3
-	sw	$8,56($fp)
+	sw	$8,44($fp)
 	lw	$8,60($fp)
-	lw	$9,56($fp)
-	add	$8,$8,$9
-	sw	$8,52($fp)
-	lw	$8,52($fp)
-	lw	$9,36($fp)
-	sub	$8,$8,$9
-	sw	$8,32($fp)
+	li	$8,3
+	sw	$8,60($fp)
 	lw	$8,32($fp)
+	lw	$9,60($fp)
+	add	$8,$8,$9
+	sw	$8,56($fp)
+	lw	$8,56($fp)
+	lw	$9,44($fp)
+	sub	$8,$8,$9
+	sw	$8,40($fp)
+	lw	$8,40($fp)
 	sw	$8,16($fp)
-	sw	$8,28($fp)
+	sw	$8,36($fp)
 	lw	$2,16($fp)
 	move	$sp,$fp
 	lw	$fp,68($sp)
--- deetz_test_28.c
+++ deetz_test_28.c
@@ -1,5 +1,7 @@
 int f(int a, int b)
 {
+	int y;
+	int x;
 	a = (y+3-(x++/12));
 	return a; 
 }
