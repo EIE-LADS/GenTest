59a848deac1b24f452c9bae5bdf01a658e2f0da0
DiederikVink <dav114@ic.ac.uk>
2016-03-17 16:20:34 +0000

    updating

--- deetz_test_26.c
+++ deetz_test_26.c
@@ -3,7 +3,7 @@ int f(int a, int b)
 	int x=3; 
 	int y=1;
 	{
-		a = (y+3-(x++/12))
+		a = (y+3-(x++/12));
 		a = (y/2+3*x-1); 
 	}
 	return a; 
--- deetz_test_28.c
+++ deetz_test_28.c
@@ -1,5 +1,5 @@
 int f(int a, int b)
 {
-	a = (y+3-(x++/12))
+	a = (y+3-(x++/12));
 	return a; 
 }
