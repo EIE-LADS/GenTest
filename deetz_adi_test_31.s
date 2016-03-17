82855099e16d924c860776bf74150bb60a45879c
DiederikVink <dav114@ic.ac.uk>
2016-03-17 16:49:32 +0000

    updating

--- deetz_adi_test_31.s
+++ deetz_adi_test_31.s
@@ -4,58 +4,46 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-84
-	sw	$31,80($sp)
-	sw	$fp,76($sp)
+	addiu	$sp,$sp,-76
+	sw	$31,72($sp)
+	sw	$fp,68($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
 	lw	$8,32($fp)
-	li	$8,1
+	li	$8,452
 	sw	$8,32($fp)
 	lw	$8,32($fp)
 	sw	$8,28($fp)
 	lw	$8,40($fp)
-	li	$8,3
+	li	$8,5
 	sw	$8,40($fp)
 	lw	$8,40($fp)
+	neg	$8,$8
+	sw	$8,44($fp)
+	lw	$8,0($fp)
 	sw	$8,36($fp)
 	lw	$8,28($fp)
-	sw	$8,44($fp)
+	sw	$8,48($fp)
 	addi	$8,$8,1
 	sw	$8,28($fp)
-	lw	$8,36($fp)
-	sw	$8,60($fp)
-	addi	$8,$8,1
-	sw	$8,36($fp)
-	lw	$8,64($fp)
-	li	$8,12
-	sw	$8,64($fp)
-	lw	$8,60($fp)
-	lw	$9,64($fp)
-	div	$8,$8,$9
-	nop
-	nop
-	sw	$8,56($fp)
-	lw	$8,40($fp)
-	li	$8,3
-	sw	$8,40($fp)
 	lw	$8,28($fp)
-	lw	$9,40($fp)
-	add	$8,$8,$9
-	sw	$8,68($fp)
-	lw	$8,68($fp)
-	lw	$9,56($fp)
-	sub	$8,$8,$9
+	lw	$9,36($fp)
+	div	$8,$8,$9
+	sw	$8,28($fp)
 	sw	$8,52($fp)
-	lw	$8,52($fp)
+	lw	$8,28($fp)
+	addi	$8,$8,-1
+	sw	$8,28($fp)
+	sw	$8,60($fp)
+	lw	$8,60($fp)
 	sw	$8,16($fp)
-	sw	$8,48($fp)
-	lw	$2,36($fp)
+	sw	$8,56($fp)
+	lw	$2,16($fp)
 	move	$sp,$fp
-	lw	$fp,76($sp)
-	lw	$31,80($sp)
-	addiu	$sp,$sp,84
+	lw	$fp,68($sp)
+	lw	$31,72($sp)
+	addiu	$sp,$sp,76
 	j	$31
 	nop
 
