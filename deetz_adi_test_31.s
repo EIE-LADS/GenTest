704b77fba97e77ee7d43d58bcc605dd9f04bc6a2
DiederikVink <dav114@ic.ac.uk>
2016-03-17 17:40:42 +0000

    updating

--- deetz_adi_test_31.s
+++ deetz_adi_test_31.s
@@ -4,14 +4,14 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-76
-	sw	$31,72($sp)
-	sw	$fp,68($sp)
+	addiu	$sp,$sp,-72
+	sw	$31,68($sp)
+	sw	$fp,64($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
 	lw	$8,32($fp)
-	li	$8,452
+	li	$8,0
 	sw	$8,32($fp)
 	lw	$8,32($fp)
 	sw	$8,28($fp)
@@ -19,31 +19,28 @@ f:
 	li	$8,5
 	sw	$8,40($fp)
 	lw	$8,40($fp)
-	neg	$8,$8
-	sw	$8,44($fp)
-	lw	$8,44($fp)
 	sw	$8,36($fp)
 	lw	$8,28($fp)
-	sw	$8,48($fp)
+	sw	$8,44($fp)
 	addi	$8,$8,1
 	sw	$8,28($fp)
 	lw	$8,28($fp)
 	lw	$9,36($fp)
 	div	$8,$8,$9
 	sw	$8,28($fp)
-	sw	$8,52($fp)
+	sw	$8,48($fp)
 	lw	$8,28($fp)
 	addi	$8,$8,-1
 	sw	$8,28($fp)
-	sw	$8,60($fp)
-	lw	$8,60($fp)
-	sw	$8,16($fp)
 	sw	$8,56($fp)
+	lw	$8,56($fp)
+	sw	$8,16($fp)
+	sw	$8,52($fp)
 	lw	$2,16($fp)
 	move	$sp,$fp
-	lw	$fp,68($sp)
-	lw	$31,72($sp)
-	addiu	$sp,$sp,76
+	lw	$fp,64($sp)
+	lw	$31,68($sp)
+	addiu	$sp,$sp,72
 	j	$31
 	nop
 
---
+++ deetz_test_31.c
@@ -0,0 +1,9 @@
+int f(int a, int b)
+{
+	int x=4; 
+	int y=1;
+	{
+		a = (y+3-(x/12))|(y/2+3*x-1); 
+	}
+	return a; 
+}
