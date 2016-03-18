b6e3e3569c18005e4f079e0cbb75ce9713f6362a
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-18 15:17:56 +0000

    dowhile_loop

--- adi_deetz_test_35.s
+++ adi_deetz_test_35.s
@@ -4,9 +4,9 @@
 	.ent	f
 	.type	f, @function
 f:
-addiu	$sp,$sp,-60
-sw	$fp,52($sp)
-sw	$31,56($sp)
+addiu	$sp,$sp,-68
+sw	$fp,60($sp)
+sw	$31,64($sp)
 move	$fp,$sp
 sw	$a0,16($fp)
 sw	$a1,20($fp)
@@ -23,11 +23,24 @@ li	$8,0
 sw	$8,44($fp)
 lw	$8,44($fp)
 sw	$8,40($fp)
+do_while_loop1:
+lw	$8,40($fp)
+sw	$8,48($fp)
+addi	$8,$8,1
+sw	$8,40($fp)
+lw	$8,28($fp)
+sw	$8,52($fp)
+addi	$8,$8,1
+sw	$8,28($fp)
+lw	$8,52($fp)
+bne	$8,$0,do_while_loop1
+nop
+end_loop1:
 lw	$2,40($fp)
 move	$sp,$fp
-lw	$fp,52($sp)
-lw	$31,56($sp)
-addiu	$sp,$sp,60
+lw	$fp,60($sp)
+lw	$31,64($sp)
+addiu	$sp,$sp,68
 j	$31
 nop
 	.end	f
