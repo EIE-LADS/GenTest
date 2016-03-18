4ca6c1263b207c4300d39f0a21cca9c086aee912
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-18 11:59:12 +0000

    while_loop

--- adi_deetz_test_34.s
+++ adi_deetz_test_34.s
@@ -10,32 +10,35 @@ sw	$31,64($sp)
 move	$fp,$sp
 sw	$a0,16($fp)
 sw	$a1,20($fp)
-lw	$8,32($fp)
+lw	$8,36($fp)
 li	$8,3
+sw	$8,36($fp)
+lw	$8,36($fp)
+neg	$8,$8
 sw	$8,32($fp)
 lw	$8,32($fp)
 sw	$8,28($fp)
-lw	$8,40($fp)
-li	$8,5
-sw	$8,40($fp)
-lw	$8,40($fp)
-sw	$8,36($fp)
-lw	$8,16($fp)
+lw	$8,44($fp)
+li	$8,0
 sw	$8,44($fp)
-addi	$8,$8,1
-sw	$8,16($fp)
-lw	$8,16($fp)
-lw	$9,20($fp)
-add	$8,$8,$9
-sw	$8,52($fp)
-lw	$8,36($fp)
-lw	$9,52($fp)
-mul	$8,$8,$9
-nop
-nop
-sw	$8,36($fp)
+lw	$8,44($fp)
+sw	$8,40($fp)
+while_loop1:
+lw	$8,28($fp)
 sw	$8,48($fp)
-lw	$2,16($fp)
+addi	$8,$8,1
+sw	$8,28($fp)
+lw	$8,48($fp)
+beq	$8,$0,end_loop1
+nop
+lw	$8,40($fp)
+sw	$8,52($fp)
+addi	$8,$8,1
+sw	$8,40($fp)
+b	while_loop1
+nop
+end_loop1:
+lw	$2,40($fp)
 move	$sp,$fp
 lw	$fp,60($sp)
 lw	$31,64($sp)
