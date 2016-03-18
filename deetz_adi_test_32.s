1a019f1e04f10505360758d56f2a9efc1537756c
DiederikVink <dav114@ic.ac.uk>
2016-03-18 11:59:40 +0000

    while

--- deetz_adi_test_32.s
+++ deetz_adi_test_32.s
@@ -11,7 +11,7 @@ f:
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
 	lw	$8,32($fp)
-	li	$8,298
+	li	$8,1106
 	sw	$8,32($fp)
 	lw	$8,32($fp)
 	sw	$8,28($fp)
@@ -20,30 +20,23 @@ f:
 	sw	$8,40($fp)
 	lw	$8,40($fp)
 	sw	$8,36($fp)
-while_start_1
-	lw	$8,36($fp)
-	addi	$8,$8,-1
-	sw	$8,36($fp)
-	sw	$8,44($fp)
-	lw	$8,44($fp)
-	beq	$8,$0,while_end_1:
-	nop
-	lw	$8,52($fp)
-	li	$8,3
-	sw	$8,52($fp)
 	lw	$8,28($fp)
-	lw	$9,52($fp)
-	rem	$8,$8,$9
+	sw	$8,44($fp)
+	addi	$8,$8,1
+	sw	$8,28($fp)
+	lw	$8,28($fp)
+	lw	$9,36($fp)
+	div	$8,$8,$9
 	sw	$8,28($fp)
 	sw	$8,48($fp)
-	b	while_start_1
-	nop
-while_end_1:
-	lw	$8,36($fp)
-	lw	$9,28($fp)
-	sllv	$8,$8,$9
+	lw	$8,28($fp)
+	addi	$8,$8,-1
+	sw	$8,28($fp)
 	sw	$8,56($fp)
-	lw	$2,56($fp)
+	lw	$8,56($fp)
+	sw	$8,16($fp)
+	sw	$8,52($fp)
+	lw	$2,16($fp)
 	move	$sp,$fp
 	lw	$fp,64($sp)
 	lw	$31,68($sp)
