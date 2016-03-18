9f1c712c8fd948d3b117286b4eac5d02f4410603
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-18 15:08:58 +0000

    dowhile_loop

---
+++ adi_deetz_test_37.s
@@ -0,0 +1,31 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f:
+addiu	$sp,$sp,-56
+sw	$fp,48($sp)
+sw	$31,52($sp)
+move	$fp,$sp
+sw	$a0,16($fp)
+sw	$a1,20($fp)
+lw	$8,36($fp)
+li	$8,3
+sw	$8,36($fp)
+lw	$8,36($fp)
+sw	$8,32($fp)
+lw	$8,28($fp)
+lw	$9,32($fp)
+add	$8,$8,$9
+sw	$8,40($fp)
+lw	$2,40($fp)
+move	$sp,$fp
+lw	$fp,48($sp)
+lw	$31,52($sp)
+addiu	$sp,$sp,56
+j	$31
+nop
+	.end	f
+	.size	f, .-f
+
