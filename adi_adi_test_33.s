274ae38217997dc6a9aaf0dcacf8a0f24d285698
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-18 16:21:43 +0000

    ifelse

--- adi_adi_test_33.s
+++ adi_adi_test_33.s
@@ -4,9 +4,9 @@
 	.ent	f
 	.type	f, @function
 f:
-addiu	$sp,$sp,-60
-sw	$fp,52($sp)
-sw	$31,56($sp)
+addiu	$sp,$sp,-92
+sw	$fp,84($sp)
+sw	$31,88($sp)
 move	$fp,$sp
 sw	$a0,16($fp)
 sw	$a1,20($fp)
@@ -20,15 +20,52 @@ li	$8,1106
 sw	$8,40($fp)
 lw	$8,40($fp)
 sw	$8,36($fp)
+if_0:
+lw	$8,52($fp)
+li	$8,10
+sw	$8,52($fp)
+lw	$8,36($fp)
+lw	$9,52($fp)
+sgt	$8,$8,$9
+sw	$8,48($fp)
+lw	$8,52($fp)
+li	$8,10
+sw	$8,52($fp)
+lw	$8,28($fp)
+lw	$9,52($fp)
+slt	$8,$8,$9
+sw	$8,56($fp)
+lw	$8,56($fp)
+lw	$9,48($fp)
+sne	$8,$8,0
+sne	$9,$9,0
+and	$8,$8,$9
+sw	$8,44($fp)
+lw	$8,44($fp)
+beq	$8,$0,endif_0
+nop
+lw	$8,64($fp)
+li	$8,2
+sw	$8,64($fp)
+lw	$8,64($fp)
+sw	$8,28($fp)
+sw	$8,60($fp)
+lw	$8,72($fp)
+li	$8,3
+sw	$8,72($fp)
+lw	$8,72($fp)
+sw	$8,36($fp)
+sw	$8,68($fp)
+else_0:
 lw	$8,28($fp)
 lw	$9,36($fp)
 add	$8,$8,$9
-sw	$8,44($fp)
-lw	$2,44($fp)
+sw	$8,76($fp)
+lw	$2,76($fp)
 move	$sp,$fp
-lw	$fp,52($sp)
-lw	$31,56($sp)
-addiu	$sp,$sp,60
+lw	$fp,84($sp)
+lw	$31,88($sp)
+addiu	$sp,$sp,92
 j	$31
 nop
 	.end	f
--- adi_adi_test_34.s
+++ adi_adi_test_34.s
@@ -4,9 +4,9 @@
 	.ent	f
 	.type	f, @function
 f:
-addiu	$sp,$sp,-56
-sw	$fp,48($sp)
-sw	$31,52($sp)
+addiu	$sp,$sp,-84
+sw	$fp,76($sp)
+sw	$31,80($sp)
 move	$fp,$sp
 sw	$a0,16($fp)
 sw	$a1,20($fp)
@@ -20,11 +20,47 @@ li	$8,1106
 sw	$8,40($fp)
 lw	$8,40($fp)
 sw	$8,36($fp)
+if_0:
+lw	$8,48($fp)
+li	$8,10
+sw	$8,48($fp)
+lw	$8,28($fp)
+lw	$9,48($fp)
+sgt	$8,$8,$9
+sw	$8,44($fp)
+lw	$8,44($fp)
+beq	$8,$0,endif_0
+nop
+lw	$8,56($fp)
+li	$8,2
+sw	$8,56($fp)
+lw	$8,56($fp)
+sw	$8,28($fp)
+sw	$8,52($fp)
+else_0:
+if_0:
+lw	$8,48($fp)
+li	$8,10
+sw	$8,48($fp)
+lw	$8,28($fp)
+lw	$9,48($fp)
+slt	$8,$8,$9
+sw	$8,60($fp)
+lw	$8,60($fp)
+beq	$8,$0,endif_0
+nop
+lw	$8,68($fp)
+li	$8,23
+sw	$8,68($fp)
+lw	$8,68($fp)
+sw	$8,28($fp)
+sw	$8,64($fp)
+else_0:
 lw	$2,28($fp)
 move	$sp,$fp
-lw	$fp,48($sp)
-lw	$31,52($sp)
-addiu	$sp,$sp,56
+lw	$fp,76($sp)
+lw	$31,80($sp)
+addiu	$sp,$sp,84
 j	$31
 nop
 	.end	f
