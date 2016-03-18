243922566e66c4904098b07f7b7fdc5fc4839f78
DiederikVink <dav114@ic.ac.uk>
2016-03-18 12:30:25 +0000

    dowhile

---
+++ deetz_deetz_test_35.s
@@ -0,0 +1,37 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-60
+	sw	$31,56($sp)
+	sw	$fp,52($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,0
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,28($fp)
+	lw	$8,40($fp)
+	li	$8,3
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	neg	$8,$8
+	sw	$8,44($fp)
+	lw	$8,44($fp)
+	sw	$8,36($fp)
+	lw	$2,28($fp)
+	move	$sp,$fp
+	lw	$fp,52($sp)
+	lw	$31,56($sp)
+	addiu	$sp,$sp,60
+	j	$31
+	nop
+
+	.end	f
+	.size	f, .-f
+
+
