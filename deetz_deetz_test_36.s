20751581ef160f92c69d529fce53d5cbf2e3f435
DiederikVink <dav114@ic.ac.uk>
2016-03-18 13:10:45 +0000

    for

---
+++ deetz_deetz_test_36.s
@@ -0,0 +1,61 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-84
+	sw	$31,80($sp)
+	sw	$fp,76($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,3
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,28($fp)
+for_start_1:
+	lw	$8,44($fp)
+	li	$8,0
+	sw	$8,44($fp)
+	lw	$8,44($fp)
+	sw	$8,36($fp)
+	sw	$8,40($fp)
+	lw	$8,52($fp)
+	li	$8,12
+	sw	$8,52($fp)
+	lw	$8,36($fp)
+	lw	$9,52($fp)
+	slt	$8,$8,$9
+	sw	$8,48($fp)
+	lw	$8,48($fp)
+	beq	$8,$0,for_end_1
+	nop
+	lw	$8,28($fp)
+	sw	$8,56($fp)
+	addi	$8,$8,1
+	sw	$8,28($fp)
+	lw	$8,36($fp)
+	sw	$8,60($fp)
+	addi	$8,$8,1
+	sw	$8,36($fp)
+	b	for_start_1
+	nop
+for_end_1:
+	lw	$8,68($fp)
+	lw	$9,28($fp)
+	add	$8,$8,$9
+	sw	$8,64($fp)
+	lw	$2,64($fp)
+	move	$sp,$fp
+	lw	$fp,76($sp)
+	lw	$31,80($sp)
+	addiu	$sp,$sp,84
+	j	$31
+	nop
+
+	.end	f
+	.size	f, .-f
+
+
---
+++ deetz_test_36.c
@@ -0,0 +1,10 @@
+int f (int a, int b)
+{
+	int a;
+	int y = 3;
+	for (a = 0; a < 12; a++)
+	{
+		y++;
+	}
+	return x + y;
+}
