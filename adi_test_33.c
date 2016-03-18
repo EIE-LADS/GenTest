b2e9ec5754b947062771c61d1ff300a6891fba9a
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-18 16:19:44 +0000

    ifelse

---
+++ adi_test_33.c
@@ -0,0 +1,11 @@
+int f(int a, int b)
+{
+	int x=5;
+	int y=0x452;
+	if(x < 10 && y > 10)
+	{
+		x = 2; 
+		y = 3;
+	}
+	return x+y; 
+}
---
+++ adi_test_34.c
@@ -0,0 +1,14 @@
+int f(int a, int b)
+{
+	int x=5;
+	int y=0x452;
+	if(x > 10)
+	{
+		x = 2; 
+	}
+	else if(x < 10)
+	{
+		x = 23;
+	}
+	return x; 
+}
