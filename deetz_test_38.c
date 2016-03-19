507db776cf880bf5585a445422e6c503d3338712
DiederikVink <dav114@ic.ac.uk>
2016-03-19 13:16:54 +0000

    deetz

---
+++ deetz_test_38.c
@@ -0,0 +1,27 @@
+int f1(int a, int b, int c);
+int f2(int a, int b, int c, int d, int e, int f);
+
+int f(int a, int b)
+{
+	a = 12;
+	f1(a, b, 33);
+}
+
+int f1(int a, int b, int c)
+{
+	a = 155;
+	int d = 12;
+	int e = 33/12;
+	int f = a + b;
+	a = f1(2, 4)
+	c = f2(a, 12, c, d, e, f);
+	return a + b + f;
+}
+
+int f2(int a, int b, int c, int d, int e, int f)
+{
+	a = b + c;
+	c = d + e;
+	f = a + c;
+	return f;
+}
