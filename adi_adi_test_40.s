11a010d9d1a1703ef95707cc57e25d0686793d23
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-19 11:40:34 +0000

    loops and ifelse

--- adi_adi_test_40.s
+++ adi_adi_test_40.s
@@ -4,9 +4,9 @@
 	.ent	f
 	.type	f, @function
 f:
-	addiu	$sp,$sp,-144
-	sw	$fp,136($sp)
-	sw	$31,140($sp)
+	addiu	$sp,$sp,-124
+	sw	$fp,116($sp)
+	sw	$31,120($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
@@ -112,43 +112,15 @@ dw_loop_2:
 	b	for_loop_1
 	nop
 end_loop_1:
-	lw	$8,112($fp)
-	li	$8,1
-	sw	$8,112($fp)
-	lw	$8,112($fp)
-	sw	$8,36($fp)
-	sw	$8,108($fp)
-for_loop_3:
-	lw	$8,80($fp)
-	li	$8,0
-	sw	$8,80($fp)
-	lw	$8,36($fp)
-	lw	$9,80($fp)
-	sge	$8,$8,$9
-	sw	$8,116($fp)
-	lw	$8,116($fp)
-	beq	$8,$0,end_loop_3
-	nop
-	lw	$8,20($fp)
-	sw	$8,120($fp)
-	addi	$8,$8,1
-	sw	$8,20($fp)
-	lw	$8,84($fp)
-	sw	$8,124($fp)
-	addi	$8,$8,-1
-	sw	$8,84($fp)
-	b	for_loop_3
-	nop
-end_loop_3:
 	lw	$8,16($fp)
 	lw	$9,20($fp)
 	add	$8,$8,$9
-	sw	$8,128($fp)
-	lw	$2,128($fp)
+	sw	$8,108($fp)
+	lw	$2,108($fp)
 	move	$sp,$fp
-	lw	$fp,136($sp)
-	lw	$31,140($sp)
-	addiu	$sp,$sp,144
+	lw	$fp,116($sp)
+	lw	$31,120($sp)
+	addiu	$sp,$sp,124
 	j	$31
 	nop
 	.end	f
