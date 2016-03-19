11c1212f689a1d668e046ea285f6f9d01fb3544b
DiederikVink <dav114@ic.ac.uk>
2016-03-19 13:59:21 +0000

    deetz

---
+++ deetz_deetz_test_39.s
@@ -0,0 +1,155 @@
+	.text
+
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-100
+	sw	$31,96($sp)
+	sw	$fp,92($sp)
+	move	$fp,$sp
+	sw	$a0,0($fp)
+	sw	$a1,4($fp)
+	sw	$a2,8($fp)
+	sw	$a3,64($fp)
+	lw	$8,104($fp)
+	sw	$8,68($fp)
+	lw	$8,80($fp)
+	li	$8,12
+	sw	$8,80($fp)
+	lw	$8,80($fp)
+	sw	$8,64($fp)
+	sw	$8,76($fp)
+	lw	$8,64($fp)
+	move	$a0,$8
+	sw	$8,0($fp)
+	lw	$8,68($fp)
+	move	$a1,$8
+	sw	$8,4($fp)
+	lw	$8,84($fp)
+	li	$8,33
+	sw	$8,84($fp)
+	lw	$8,84($fp)
+	move	$a2,$8
+	sw	$8,8($fp)
+	jal	f1
+	nop
+	move	$2,$0
+	move	$sp,$fp
+	lw	$fp,92($sp)
+	lw	$31,96($sp)
+	addiu	$sp,$sp,100
+	j	$31
+	nop
+
+	.end	f
+	.size	f, .-f
+
+	.align	2
+	.globl	f1
+	.ent	f1
+	.type	f1, @function
+f1: 
+	addiu	$sp,$sp,-164
+	sw	$31,160($sp)
+	sw	$fp,156($sp)
+	move	$fp,$sp
+	sw	$a0,64($fp)
+	sw	$a1,68($fp)
+	sw	$a2,72($fp)
+	lw	$8,64($fp)
+	lw	$9,68($fp)
+	add	$8,$8,$9
+	sw	$8,84($fp)
+	lw	$8,84($fp)
+	sw	$8,80($fp)
+	lw	$8,96($fp)
+	li	$8,33
+	sw	$8,96($fp)
+	lw	$8,100($fp)
+	li	$8,12
+	sw	$8,100($fp)
+	lw	$8,96($fp)
+	lw	$9,100($fp)
+	div	$8,$8,$9
+	nop
+	nop
+	sw	$8,92($fp)
+	lw	$8,92($fp)
+	sw	$8,88($fp)
+	lw	$8,108($fp)
+	li	$8,12
+	sw	$8,108($fp)
+	lw	$8,108($fp)
+	sw	$8,104($fp)
+	lw	$8,116($fp)
+	li	$8,155
+	sw	$8,116($fp)
+	lw	$8,116($fp)
+	sw	$8,64($fp)
+	sw	$8,112($fp)
+	lw	$8,124($fp)
+	li	$8,2
+	sw	$8,124($fp)
+	lw	$8,124($fp)
+	move	$a0,$8
+	sw	$8,0($fp)
+	lw	$8,128($fp)
+	li	$8,4
+	sw	$8,128($fp)
+	lw	$8,128($fp)
+	move	$a1,$8
+	sw	$8,4($fp)
+	lw	$8,80($fp)
+	move	$a2,$8
+	sw	$8,8($fp)
+	jal	f1
+	nop
+	lw	$8,0($fp)
+	sw	$8,64($fp)
+	sw	$8,120($fp)
+	lw	$8,64($fp)
+	move	$a0,$8
+	sw	$8,0($fp)
+	lw	$8,140($fp)
+	li	$8,12
+	sw	$8,140($fp)
+	lw	$8,140($fp)
+	move	$a1,$8
+	sw	$8,4($fp)
+	lw	$8,72($fp)
+	move	$a2,$8
+	sw	$8,8($fp)
+	lw	$8,104($fp)
+	move	$a3,$8
+	sw	$8,12($fp)
+	lw	$8,88($fp)
+	sw	$8,16($fp)
+	lw	$8,80($fp)
+	sw	$8,20($fp)
+	jal	f2
+	nop
+	lw	$8,0($fp)
+	sw	$8,72($fp)
+	sw	$8,132($fp)
+	lw	$8,64($fp)
+	lw	$9,68($fp)
+	add	$8,$8,$9
+	sw	$8,148($fp)
+	lw	$8,148($fp)
+	lw	$9,80($fp)
+	add	$8,$8,$9
+	sw	$8,144($fp)
+	lw	$2,144($fp)
+	move	$sp,$fp
+	lw	$fp,156($sp)
+	lw	$31,160($sp)
+	addiu	$sp,$sp,164
+	j	$31
+	nop
+
+	.end	f1
+	.size	f1, .-f1
+
+
---
+++ deetz_test_39.c
@@ -0,0 +1,20 @@
+int f1(int a, int b, int c);
+
+int f(int a, int b)
+{
+	a = 12;
+	return f1(a, b, 33);
+	
+}
+
+int f1(int a, int b, int c)
+{
+	int d = 12;
+	int e = 33/12;
+	int f = a + b;
+	a = 155;
+	a = f1(2, 4, f);
+	c = f2(a, 12, c, d, e, f);
+	return a + b + f;
+}
+
