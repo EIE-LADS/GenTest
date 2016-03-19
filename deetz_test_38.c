949a2f20f49ad014c4971162bf6958cfaea12fe0
DiederikVink <dav114@ic.ac.uk>
2016-03-19 13:32:44 +0000

    deetz

--- deetz_test_38.c
+++ deetz_test_38.c
@@ -4,7 +4,8 @@ int f2(int a, int b, int c, int d, int e, int f);
 int f(int a, int b)
 {
 	a = 12;
-	f1(a, b, 33);
+	return f1(a, b, 33);
+	
 }
 
 int f1(int a, int b, int c)
@@ -13,7 +14,7 @@ int f1(int a, int b, int c)
 	int d = 12;
 	int e = 33/12;
 	int f = a + b;
-	a = f1(2, 4, f)
+	a = f1(2, 4, f);
 	c = f2(a, 12, c, d, e, f);
 	return a + b + f;
 }
