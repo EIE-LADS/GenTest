06890e83ee550610ba47c5b33540cc3c18469c4f
DiederikVink <dav114@ic.ac.uk>
2016-03-19 11:59:05 +0000

    deetz

--- deetz_adi_test_40.s
+++ deetz_adi_test_40.s
@@ -4,9 +4,9 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-124
-	sw	$31,120($sp)
-	sw	$fp,116($sp)
+	addiu	$sp,$sp,-144
+	sw	$31,140($sp)
+	sw	$fp,136($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
@@ -114,15 +114,43 @@ do_while_start_1:
 	b	for_start_1
 	nop
 for_end_1:
+	lw	$8,112($fp)
+	li	$8,1
+	sw	$8,112($fp)
+	lw	$8,112($fp)
+	sw	$8,28($fp)
+	sw	$8,108($fp)
+for_start_2:
+	lw	$8,80($fp)
+	li	$8,0
+	sw	$8,80($fp)
+	lw	$8,28($fp)
+	lw	$9,80($fp)
+	sge	$8,$8,$9
+	sw	$8,116($fp)
+	lw	$8,116($fp)
+	beq	$8,$0,for_end_2
+	nop
+	lw	$8,20($fp)
+	sw	$8,120($fp)
+	addi	$8,$8,1
+	sw	$8,20($fp)
+	lw	$8,84($fp)
+	addi	$8,$8,-1
+	sw	$8,84($fp)
+	sw	$8,124($fp)
+	b	for_start_2
+	nop
+for_end_2:
 	lw	$8,16($fp)
 	lw	$9,20($fp)
 	add	$8,$8,$9
-	sw	$8,108($fp)
-	lw	$2,108($fp)
+	sw	$8,128($fp)
+	lw	$2,128($fp)
 	move	$sp,$fp
-	lw	$fp,116($sp)
-	lw	$31,120($sp)
-	addiu	$sp,$sp,124
+	lw	$fp,136($sp)
+	lw	$31,140($sp)
+	addiu	$sp,$sp,144
 	j	$31
 	nop
 
