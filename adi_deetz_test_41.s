7e13237aaf8a4211e2dc2f5c9fb48693f38f83be
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-20 15:11:18 +0000

    udp final

--- adi_deetz_test_41.s
+++ adi_deetz_test_41.s
@@ -42,28 +42,42 @@ f:
 	.ent	f1
 	.type	f1, @function
 f1:
-	addiu	$sp,$sp,-48
-	sw	$fp,40($sp)
-	sw	$31,44($sp)
+	addiu	$sp,$sp,-64
+	sw	$fp,56($sp)
+	sw	$31,60($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
 	sw	$a2,24($fp)
+	lw	$8,32($fp)
+	li	$8,13
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,16($fp)
+	sw	$8,28($fp)
+	lw	$8,40($fp)
+	li	$8,12
+	sw	$8,40($fp)
+	lw	$8,20($fp)
+	lw	$9,40($fp)
+	add	$8,$8,$9
+	sw	$8,20($fp)
+	sw	$8,36($fp)
 	lw	$8,16($fp)
 	lw	$9,20($fp)
 	add	$8,$8,$9
-	sw	$8,32($fp)
-	lw	$8,32($fp)
+	sw	$8,48($fp)
+	lw	$8,48($fp)
 	lw	$9,24($fp)
 	add	$8,$8,$9
-	sw	$8,28($fp)
-	lw	$2,28($fp)
+	sw	$8,44($fp)
+	lw	$2,44($fp)
 	move	$sp,$fp
-	lw	$fp,40($sp)
-	lw	$31,44($sp)
-	addiu	$sp,$sp,48
+	lw	$fp,56($sp)
+	lw	$31,60($sp)
+	addiu	$sp,$sp,64
 	j	$31
 	nop
 	.end	f1
