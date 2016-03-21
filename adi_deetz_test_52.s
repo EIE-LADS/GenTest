a010868f5a9b2ff661e41dac49f21d7fe6ac5f02
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-21 23:15:40 +0000

    array test

--- adi_deetz_test_52.s
+++ adi_deetz_test_52.s
@@ -4,9 +4,9 @@
 	.ent	f
 	.type	f, @function
 f:
-	addiu	$sp,$sp,-80
-	sw	$fp,72($sp)
-	sw	$31,76($sp)
+	addiu	$sp,$sp,-84
+	sw	$fp,76($sp)
+	sw	$31,80($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
@@ -29,35 +29,35 @@ f:
 	lw	$8,48($fp)
 	sw	$8,40($fp)
 	sw	$8,36($fp)
-	lw	$8,32($fp)
-	li	$8,11
-	sw	$8,32($fp)
-	lw	$8,32($fp)
-	la	$11,arr_28
-	sll	$8,$8,2
-	add	$11,$11,$8
-	lw	$8,0($11)
-	sw	$8,56($fp)
 	lw	$8,60($fp)
-	li	$8,4
+	li	$8,10
 	sw	$8,60($fp)
 	lw	$8,60($fp)
-	sw	$8,56($fp)
-	sw	$8,52($fp)
-	lw	$8,32($fp)
-	li	$8,11
-	sw	$8,32($fp)
-	lw	$8,32($fp)
 	la	$11,arr_28
 	sll	$8,$8,2
 	add	$11,$11,$8
 	lw	$8,0($11)
+	sw	$8,56($fp)
+	lw	$8,64($fp)
+	li	$8,4
 	sw	$8,64($fp)
-	lw	$2,64($fp)
+	lw	$8,64($fp)
+	sw	$8,56($fp)
+	sw	$8,52($fp)
+	lw	$8,60($fp)
+	li	$8,10
+	sw	$8,60($fp)
+	lw	$8,60($fp)
+	la	$11,arr_28
+	sll	$8,$8,2
+	add	$11,$11,$8
+	lw	$8,0($11)
+	sw	$8,68($fp)
+	lw	$2,68($fp)
 	move	$sp,$fp
-	lw	$fp,72($sp)
-	lw	$31,76($sp)
-	addiu	$sp,$sp,80
+	lw	$fp,76($sp)
+	lw	$31,80($sp)
+	addiu	$sp,$sp,84
 	j	$31
 	nop
 	.end	f
