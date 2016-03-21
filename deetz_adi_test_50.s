b9d1f88298415ff2bab0def61d67a3d7bf099550
DiederikVink <dav114@ic.ac.uk>
2016-03-21 18:41:32 +0000

    deetz

--- deetz_adi_test_50.s
+++ deetz_adi_test_50.s
@@ -4,17 +4,78 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-44
-	sw	$31,40($sp)
-	sw	$fp,36($sp)
+	addiu	$sp,$sp,-80
+	sw	$31,76($sp)
+	sw	$fp,72($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
+	lw	$12,16($fp)
+case_test_0: 
+	lw	$8,32($fp)
+	li	$8,0
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	bne	$8,$12,case_test_1
+	nop
+case_body_0:
+	lw	$8,40($fp)
+	li	$8,1
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	sw	$8,28($fp)
+	sw	$8,36($fp)
+	b	case_body_1
+	nop
+case_test_1: 
+	lw	$8,40($fp)
+	li	$8,1
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	bne	$8,$12,case_test_2
+	nop
+case_body_1:
+	lw	$8,48($fp)
+	li	$8,2
+	sw	$8,48($fp)
+	lw	$8,48($fp)
+	sw	$8,28($fp)
+	sw	$8,44($fp)
+	b	case_body_2
+	nop
+case_test_2: 
+	lw	$8,52($fp)
+	li	$8,3
+	sw	$8,52($fp)
+	lw	$8,52($fp)
+	bne	$8,$12,case_test_3
+	nop
+case_body_2:
+	lw	$8,52($fp)
+	li	$8,3
+	sw	$8,52($fp)
+	lw	$8,52($fp)
+	sw	$8,28($fp)
+	sw	$8,56($fp)
+	b	case_body_3
+	nop
+case_test_3: 
+case_body_3: 
+	lw	$8,40($fp)
+	li	$8,1
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	neg	$8,$8
+	sw	$8,64($fp)
+	lw	$8,64($fp)
+	sw	$8,28($fp)
+	sw	$8,60($fp)
+break_exit_1
 	lw	$2,28($fp)
 	move	$sp,$fp
-	lw	$fp,36($sp)
-	lw	$31,40($sp)
-	addiu	$sp,$sp,44
+	lw	$fp,72($sp)
+	lw	$31,76($sp)
+	addiu	$sp,$sp,80
 	j	$31
 	nop
 	.end	f
--- deetz_adi_test_52.s
+++ deetz_adi_test_52.s
@@ -4,9 +4,9 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-48
-	sw	$31,44($sp)
-	sw	$fp,40($sp)
+	addiu	$sp,$sp,-88
+	sw	$31,84($sp)
+	sw	$fp,80($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
@@ -15,11 +15,87 @@ f:
 	sw	$8,32($fp)
 	lw	$8,32($fp)
 	sw	$8,28($fp)
+	lw	$12,28($fp)
+case_test_0: 
+	lw	$8,32($fp)
+	li	$8,1
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	bne	$8,$12,case_test_1
+	nop
+case_body_0:
+	lw	$8,40($fp)
+	li	$8,3
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	sw	$8,36($fp)
+while_start_1:
+	lw	$8,48($fp)
+	li	$8,4
+	sw	$8,48($fp)
+	lw	$8,48($fp)
+	neg	$8,$8
+	sw	$8,52($fp)
+	lw	$8,36($fp)
+	lw	$9,52($fp)
+	sge	$8,$8,$9
+	sw	$8,44($fp)
+	lw	$8,44($fp)
+	beq	$8,$0,while_end_1
+	nop
+	lw	$8,16($fp)
+	sw	$8,56($fp)
+	addi	$8,$8,1
+	sw	$8,16($fp)
+	lw	$8,36($fp)
+	addi	$8,$8,-1
+	sw	$8,36($fp)
+	sw	$8,60($fp)
+	b	break_exit_2
+	nop
+	b	while_start_1
+	nop
+while_end_1:
+break_exit_2: 
+	b	break_exit_1
+	nop
+	b	case_body_1
+	nop
+case_test_1: 
+	lw	$8,64($fp)
+	li	$8,2
+	sw	$8,64($fp)
+	lw	$8,64($fp)
+	bne	$8,$12,case_test_2
+	nop
+case_body_1:
+	b	break_exit_1
+	nop
+	b	case_body_2
+	nop
+case_test_2: 
+	lw	$8,68($fp)
+	li	$8,3
+	sw	$8,68($fp)
+	lw	$8,68($fp)
+	bne	$8,$12,case_test_3
+	nop
+case_body_2:
+	lw	$8,16($fp)
+	lw	$9,20($fp)
+	add	$8,$8,$9
+	sw	$8,16($fp)
+	sw	$8,72($fp)
+	b	break_exit_1
+	nop
+	b	case_body_3
+	nop
+break_exit_1
 	lw	$2,16($fp)
 	move	$sp,$fp
-	lw	$fp,40($sp)
-	lw	$31,44($sp)
-	addiu	$sp,$sp,48
+	lw	$fp,80($sp)
+	lw	$31,84($sp)
+	addiu	$sp,$sp,88
 	j	$31
 	nop
 	.end	f
