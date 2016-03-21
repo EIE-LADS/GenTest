b635bbc90ec8715b5318ba87a72b35ffc2a15d70
DiederikVink <dav114@ic.ac.uk>
2016-03-21 23:13:05 +0000

    deetz

---
+++ deetz_deetz_test_52.s
@@ -0,0 +1,75 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-80
+	sw	$31,76($sp)
+	sw	$fp,72($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,11
+	sw	$8,32($fp)
+	la	$11,array_data_28
+	lw	$8,44($fp)
+	li	$8,3
+	sw	$8,44($fp)
+	la	$11,array_data_28
+	lw	$8,44($fp)
+	li	$9,4
+	mul	$8,$8,$9
+	nop
+	add	$11,$11,$8
+	lw	$8,0($11)
+	sw	$8,40($fp)
+	lw	$8,48($fp)
+	li	$8,12
+	sw	$8,48($fp)
+	lw	$8,48($fp)
+	sw	$8,40($fp)
+	sw	$8,36($fp)
+	lw	$8,32($fp)
+	li	$8,11
+	sw	$8,32($fp)
+	la	$11,array_data_28
+	lw	$8,32($fp)
+	li	$9,4
+	mul	$8,$8,$9
+	nop
+	add	$11,$11,$8
+	lw	$8,0($11)
+	sw	$8,56($fp)
+	lw	$8,60($fp)
+	li	$8,4
+	sw	$8,60($fp)
+	lw	$8,60($fp)
+	sw	$8,56($fp)
+	sw	$8,52($fp)
+	lw	$8,32($fp)
+	li	$8,11
+	sw	$8,32($fp)
+	la	$11,array_data_28
+	lw	$8,32($fp)
+	li	$9,4
+	mul	$8,$8,$9
+	nop
+	add	$11,$11,$8
+	lw	$8,0($11)
+	sw	$8,64($fp)
+	lw	$2,64($fp)
+	move	$sp,$fp
+	lw	$fp,72($sp)
+	lw	$31,76($sp)
+	addiu	$sp,$sp,80
+	j	$31
+	nop
+	.end	f
+	.size	f, .-f
+
+.data
+array_data_28:	.word	
+
+
---
+++ deetz_test_52.c
@@ -0,0 +1,7 @@
+int f(int a, int b)
+{
+	int array[11];
+	array[3] = 12;
+	array[11] = 4;
+	return array[11]; 
+}
