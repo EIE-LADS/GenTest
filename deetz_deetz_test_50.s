6d529773239d2170f09e120b94a1193e37d40ca8
DiederikVink <dav114@ic.ac.uk>
2016-03-21 21:52:10 +0000

    deetz

--- deetz_deetz_test_50.s
+++ deetz_deetz_test_50.s
@@ -4,16 +4,16 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-68
-	sw	$31,64($sp)
-	sw	$fp,60($sp)
+	addiu	$sp,$sp,-80
+	sw	$31,76($sp)
+	sw	$fp,72($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
 	lw	$8,28($fp)
 	li	$8,4
 	sw	$8,28($fp)
-	la	$11,array_data_28($fp)
+	la	$11,array_data_28
 	lw	$8,36($fp)
 	li	$8,3
 	sw	$8,36($fp)
@@ -23,31 +23,44 @@ f:
 	li	$8,4
 	sw	$8,28($fp)
 	lw	$8,28($fp)
-	sw	$8,1($11)
+	sw	$8,4($11)
 	lw	$8,40($fp)
 	li	$8,6
 	sw	$8,40($fp)
 	lw	$8,40($fp)
-	sw	$8,2($11)
+	sw	$8,8($11)
 	lw	$8,44($fp)
 	li	$8,12
 	sw	$8,44($fp)
 	lw	$8,44($fp)
-	sw	$8,3($11)
+	sw	$8,12($11)
 	lw	$8,36($fp)
 	sw	$8,32($fp)
-	lw	$8,52($fp)
-	li	$8,2
-	sw	$8,52($fp)
-	lw	$8,52($fp)
+	lw	$8,36($fp)
+	li	$8,3
+	sw	$8,36($fp)
+	lw	$8,36($fp)
 	la	$11,32($fp)
 	add	$11,$11,$8
-	sw	$11,48($fp)
-	lw	$2,48($fp)
+	sw	$11,52($fp)
+	lw	$8,56($fp)
+	li	$8,13
+	sw	$8,56($fp)
+	lw	$8,56($fp)
+	sw	$8,52($fp)
+	sw	$8,48($fp)
+	lw	$8,64($fp)
+	li	$8,2
+	sw	$8,64($fp)
+	lw	$8,64($fp)
+	la	$11,32($fp)
+	add	$11,$11,$8
+	sw	$11,60($fp)
+	lw	$2,60($fp)
 	move	$sp,$fp
-	lw	$fp,60($sp)
-	lw	$31,64($sp)
-	addiu	$sp,$sp,68
+	lw	$fp,72($sp)
+	lw	$31,76($sp)
+	addiu	$sp,$sp,80
 	j	$31
 	nop
 	.end	f
