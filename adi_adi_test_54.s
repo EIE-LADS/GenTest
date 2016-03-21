4fde5de7c51963103d5f85a931f691cfcc528cdf
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-21 23:10:59 +0000

    array test

--- adi_adi_test_54.s
+++ adi_adi_test_54.s
@@ -4,14 +4,14 @@
 	.ent	f
 	.type	f, @function
 f:
-	addiu	$sp,$sp,-88
-	sw	$fp,80($sp)
-	sw	$31,84($sp)
+	addiu	$sp,$sp,-68
+	sw	$fp,60($sp)
+	sw	$31,64($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
 	lw	$8,32($fp)
-	li	$8,10
+	li	$8,5
 	sw	$8,32($fp)
 	la	$11,arr_28
 	lw	$8,36($fp)
@@ -34,36 +34,11 @@ f:
 	sw	$8,48($fp)
 	lw	$8,48($fp)
 	sw	$8,12($11)
-	lw	$8,52($fp)
-	li	$8,5
-	sw	$8,52($fp)
-	lw	$8,52($fp)
-	sw	$8,16($11)
-	lw	$8,56($fp)
-	li	$8,6
-	sw	$8,56($fp)
-	lw	$8,56($fp)
-	sw	$8,20($11)
-	lw	$8,60($fp)
-	li	$8,7
-	sw	$8,60($fp)
-	lw	$8,60($fp)
-	sw	$8,24($11)
-	lw	$8,64($fp)
-	li	$8,8
-	sw	$8,64($fp)
-	lw	$8,64($fp)
-	sw	$8,28($11)
-	lw	$8,68($fp)
-	li	$8,9
-	sw	$8,68($fp)
-	lw	$8,68($fp)
-	sw	$8,32($11)
 	lw	$8,32($fp)
-	li	$8,10
+	li	$8,5
 	sw	$8,32($fp)
 	lw	$8,32($fp)
-	sw	$8,36($11)
+	sw	$8,16($11)
 	lw	$8,36($fp)
 	sw	$8,28($fp)
 	lw	$8,44($fp)
@@ -74,12 +49,12 @@ f:
 	sll	$8,$8,2
 	add	$11,$11,$8
 	lw	$8,0($11)
-	sw	$8,72($fp)
-	lw	$2,72($fp)
+	sw	$8,52($fp)
+	lw	$2,52($fp)
 	move	$sp,$fp
-	lw	$fp,80($sp)
-	lw	$31,84($sp)
-	addiu	$sp,$sp,88
+	lw	$fp,60($sp)
+	lw	$31,64($sp)
+	addiu	$sp,$sp,68
 	j	$31
 	nop
 	.end	f
--- adi_test_54.c
+++ adi_test_54.c
@@ -1,5 +1,5 @@
 int f(int a, int b)
 {
-	int array[10] = {1,2,3,4,5,6,7,8,9,10}; 
+	int array[5] = {1,2,3,4,5}; 
 	return array[3]; 
 }
