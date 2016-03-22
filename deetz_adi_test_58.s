7f202fe923d693cdfc7e059b9c9ac8313aeed0f6
DiederikVink <dav114@ic.ac.uk>
2016-03-22 20:01:35 +0000

    deetz

--- deetz_adi_test_58.s
+++ deetz_adi_test_58.s
@@ -131,37 +131,54 @@ f:
 	.ent	f1
 	.type	f1, @function
 f1: 
-	addiu	$sp,$sp,-56
-	sw	$31,52($sp)
-	sw	$fp,48($sp)
+	addiu	$sp,$sp,-72
+	sw	$31,68($sp)
+	sw	$fp,64($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
-	lw	$t0,32($fp)
+	lw	$t0,36($fp)
 	li	$t0,2
-	sw	$t0,32($fp)
+	sw	$t0,36($fp)
 	lw	$t3,16($fp)
-	lw	$t0,32($fp)
+	lw	$t0,36($fp)
+	li	$t1,4
+	mul	$t0,$t0,$t1
+	nop
+	nop
+	add	$t3,$t3,$t0
+	sw	$t3,32($fp)
+	lw	$t0,44($fp)
+	li	$t0,1
+	sw	$t0,44($fp)
+	lw	$t0,52($fp)
+	li	$t0,2
+	sw	$t0,52($fp)
+	lw	$t3,16($fp)
+	lw	$t0,52($fp)
 	li	$t1,4
 	mul	$t0,$t0,$t1
 	nop
 	nop
 	add	$t3,$t3,$t0
 	lw	$t1,0($t3)
-	sw	$t1,28($fp)
-	lw	$t3,28($fp)
-	lw	$t0,0($t3)
-	sw	$t0,36($fp)
-	addi	$t0,$t0,1
-	sw	$t0,0($t3)
-	lw	$t0,40($fp)
-	li	$t0,0
+	sw	$t1,48($fp)
+	lw	$t0,48($fp)
+	lw	$t1,44($fp)
+	add	$t0,$t0,$t1
 	sw	$t0,40($fp)
-	lw	$2,40($fp)
+	lw	$t0,40($fp)
+	lw	$t1,32($fp)
+	sw	$t0,0($t1)
+	sw	$t0,28($fp)
+	lw	$t0,56($fp)
+	li	$t0,0
+	sw	$t0,56($fp)
+	lw	$2,56($fp)
 	move	$sp,$fp
-	lw	$fp,48($sp)
-	lw	$31,52($sp)
-	addiu	$sp,$sp,56
+	lw	$fp,64($sp)
+	lw	$31,68($sp)
+	addiu	$sp,$sp,72
 	j	$31
 	nop
 	.end	f1
