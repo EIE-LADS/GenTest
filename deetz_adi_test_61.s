40ffbd3d8a85c8bc7610062ac998dcb60de5307f
DiederikVink <dav114@ic.ac.uk>
2016-03-22 23:26:17 +0000

    deetz

--- deetz_adi_test_61.s
+++ deetz_adi_test_61.s
@@ -4,9 +4,9 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-64
-	sw	$31,60($sp)
-	sw	$fp,56($sp)
+	addiu	$sp,$sp,-68
+	sw	$31,64($sp)
+	sw	$fp,60($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
@@ -35,9 +35,20 @@ f:
 	nop
 	nop
 	add	$t3,$t3,$t0
+	sw	$t3,40($fp)
+	lw	$t0,36($fp)
+	li	$t0,1
+	sw	$t0,36($fp)
+	lw	$t3,28($fp)
+	lw	$t0,36($fp)
+	li	$t1,4
+	mul	$t0,$t0,$t1
+	nop
+	nop
+	add	$t3,$t3,$t0
 	sw	$t3,44($fp)
 	lw	$t0,44($fp)
-	sw	$t0,40($fp)
+	sw	$t0,48($fp)
 	addi	$t0,$t0,1
 	sw	$t0,44($fp)
 	lw	$t0,36($fp)
@@ -51,12 +62,12 @@ f:
 	nop
 	add	$t3,$t3,$t0
 	lw	$t1,0($t3)
-	sw	$t1,48($fp)
-	lw	$2,48($fp)
+	sw	$t1,52($fp)
+	lw	$2,52($fp)
 	move	$sp,$fp
-	lw	$fp,56($sp)
-	lw	$31,60($sp)
-	addiu	$sp,$sp,64
+	lw	$fp,60($sp)
+	lw	$31,64($sp)
+	addiu	$sp,$sp,68
 	j	$31
 	nop
 	.end	f
