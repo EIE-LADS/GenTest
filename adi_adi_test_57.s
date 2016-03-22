c891e40a6ec8f7f5becb3ccc897391708295887f
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-22 15:10:42 +0000

    dt tests

--- adi_adi_test_57.s
+++ adi_adi_test_57.s
@@ -10,31 +10,31 @@ f:
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
-	lw	$8,16($fp)
-	lw	$9,20($fp)
-	sgt	$8,$8,$9
-	sw	$8,32($fp)
+	lw	$8,40($fp)
+	li	$8,1
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	neg	$8,$8
+	sw	$8,36($fp)
+	lw	$8,40($fp)
+	li	$8,1
+	sw	$8,40($fp)
 	lw	$8,16($fp)
 	lw	$9,20($fp)
 	seq	$8,$8,$9
-	sw	$8,40($fp)
-	lw	$8,48($fp)
-	li	$8,1
 	sw	$8,48($fp)
 	lw	$8,48($fp)
-	neg	$8,$8
-	sw	$8,44($fp)
-	lw	$8,48($fp)
-	li	$8,1
-	sw	$8,48($fp)
-	lw	$8,40($fp)
-	lw	$9,44($fp)
-	lw	$10,52($fp)
+	lw	$9,36($fp)
+	lw	$10,44($fp)
 	movz	$9,$10,$8
-	sw	$9,36($fp)
-	lw	$8,32($fp)
+	sw	$9,32($fp)
+	lw	$8,16($fp)
+	lw	$9,20($fp)
+	sgt	$8,$8,$9
+	sw	$8,52($fp)
+	lw	$8,52($fp)
 	lw	$9,16($fp)
-	lw	$10,36($fp)
+	lw	$10,32($fp)
 	movz	$9,$10,$8
 	sw	$9,28($fp)
 	lw	$2,28($fp)
