6cb857289ee07ad960527d05a09eee313493ab3b
DiederikVink <dav114@ic.ac.uk>
2016-03-22 11:25:21 +0000

    deetz

---
+++ deetz_adi_test_56.s
@@ -0,0 +1,119 @@
+	.text
+
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-76
+	sw	$31,72($sp)
+	sw	$fp,68($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,3
+	sw	$8,32($fp)
+	la	$11,array_data_28
+	lw	$8,32($fp)
+	li	$8,3
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,0($11)
+	lw	$8,36($fp)
+	li	$8,10
+	sw	$8,36($fp)
+	lw	$8,36($fp)
+	neg	$8,$8
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	sw	$8,4($11)
+	lw	$8,44($fp)
+	li	$8,1
+	sw	$8,44($fp)
+	lw	$8,44($fp)
+	sw	$8,8($11)
+	lw	$8,32($fp)
+	sw	$8,28($fp)
+	lw	$8,28($fp)
+	move	$a0,$8
+	sw	$8,0($fp)
+	la	$8,f1
+	jalr	$8
+	nop
+	sw	$2,52($fp)
+	lw	$8,52($fp)
+	sw	$8,48($fp)
+	lw	$8,60($fp)
+	li	$8,2
+	sw	$8,60($fp)
+	la	$11,array_data_28
+	lw	$8,60($fp)
+	li	$9,4
+	mul	$8,$8,$9
+	nop
+	add	$11,$11,$8
+	lw	$9,0($11)
+	sw	$9,56($fp)
+	lw	$2,56($fp)
+	move	$sp,$fp
+	lw	$fp,68($sp)
+	lw	$31,72($sp)
+	addiu	$sp,$sp,76
+	j	$31
+	nop
+	.end	f
+	.size	f, .-f
+
+	.align	2
+	.globl	f1
+	.ent	f1
+	.type	f1, @function
+f1: 
+	addiu	$sp,$sp,-60
+	sw	$31,56($sp)
+	sw	$fp,52($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	lw	$8,32($fp)
+	li	$8,2
+	sw	$8,32($fp)
+	la	$11,array_data_16
+	lw	$8,32($fp)
+	li	$9,4
+	mul	$8,$8,$9
+	nop
+	add	$11,$11,$8
+	sw	$11,28($fp)
+	lw	$8,36($fp)
+	li	$8,80
+	sw	$8,36($fp)
+	lw	$8,36($fp)
+	lw	$9,28($fp)
+	sw	$8,0($9)
+	sw	$8,24($fp)
+	lw	$8,44($fp)
+	li	$8,2
+	sw	$8,44($fp)
+	la	$11,array_data_16
+	lw	$8,44($fp)
+	li	$9,4
+	mul	$8,$8,$9
+	nop
+	add	$11,$11,$8
+	lw	$9,0($11)
+	sw	$9,40($fp)
+	lw	$2,40($fp)
+	move	$sp,$fp
+	lw	$fp,52($sp)
+	lw	$31,56($sp)
+	addiu	$sp,$sp,60
+	j	$31
+	nop
+	.end	f1
+	.size	f1, .-f1
+
+.data
+
+array_data_28:	.word	0,0,0,
+
