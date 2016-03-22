c463a7b562ff039f8c5beea142115f36f9073624
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-22 20:07:57 +0000

    array function passing

---
+++ adi_test_60.c
@@ -0,0 +1,20 @@
+int f1(int a[], int b);
+
+int f(int a, int b)
+{
+	int array[4];
+	int x;
+	array[0] = 1;
+	array[2] = 2; 
+	array[1] = 10; 
+	array[3] = -50;
+	x = f1(array, a); 
+	return array[2]; 
+}
+
+int f1(int a[], int b)
+{
+	a[2]++;
+	return 0; 
+}
+
