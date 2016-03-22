73be5c32dcf5f150c3b3907fc4be2f227ac9f0d4
DiederikVink <dav114@ic.ac.uk>
2016-03-22 18:28:38 +0000

    deetz

--- deetz_deetz_test_59.s
+++ deetz_deetz_test_59.s
@@ -5,9 +5,9 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-104
-	sw	$31,100($sp)
-	sw	$fp,96($sp)
+	addiu	$sp,$sp,-52
+	sw	$31,48($sp)
+	sw	$fp,44($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
@@ -16,97 +16,125 @@ f:
 	sw	$t0,32($fp)
 	la	$t3,array_data_T_28
 	sw	$t3,28($fp)
-	lw	$t0,48($fp)
-	li	$t0,0
-	sw	$t0,48($fp)
-	lw	$t3,28($fp)
-	lw	$t0,48($fp)
-	li	$t1,4
-	mul	$t0,$t0,$t1
+	lw	$t0,28($fp)
+	move	$a0,$t0
+	sw	$t0,0($fp)
+	la	$t0,f1
+	jalr	$t0
 	nop
-	nop
-	add	$t3,$t3,$t0
-	sw	$t3,44($fp)
-	lw	$t0,32($fp)
-	li	$t0,4
-	sw	$t0,32($fp)
-	lw	$t0,32($fp)
-	lw	$t1,44($fp)
-	sw	$t0,0($t1)
-	sw	$t0,40($fp)
-	lw	$t0,60($fp)
-	li	$t0,1
-	sw	$t0,60($fp)
-	lw	$t3,28($fp)
-	lw	$t0,60($fp)
-	li	$t1,4
-	mul	$t0,$t0,$t1
-	nop
-	nop
-	add	$t3,$t3,$t0
-	sw	$t3,56($fp)
-	lw	$t0,48($fp)
-	li	$t0,0
-	sw	$t0,48($fp)
-	lw	$t3,28($fp)
-	lw	$t0,48($fp)
-	li	$t1,4
-	mul	$t0,$t0,$t1
-	nop
-	nop
-	add	$t3,$t3,$t0
-	lw	$t1,0($t3)
-	sw	$t1,64($fp)
-	lw	$t0,64($fp)
-	lw	$t1,56($fp)
-	sw	$t0,0($t1)
-	sw	$t0,52($fp)
-	lw	$t0,48($fp)
-	li	$t0,0
-	sw	$t0,48($fp)
-	lw	$t3,28($fp)
-	lw	$t0,48($fp)
-	li	$t1,4
-	mul	$t0,$t0,$t1
-	nop
-	nop
-	add	$t3,$t3,$t0
-	sw	$t3,76($fp)
-	lw	$t0,80($fp)
-	li	$t0,8
-	sw	$t0,80($fp)
-	lw	$t0,80($fp)
-	lw	$t1,76($fp)
-	sw	$t0,0($t1)
-	sw	$t0,72($fp)
-	lw	$t0,72($fp)
-	sw	$t0,36($fp)
-	sw	$t0,68($fp)
-	lw	$t0,48($fp)
-	li	$t0,0
-	sw	$t0,48($fp)
-	lw	$t3,28($fp)
-	lw	$t0,48($fp)
-	li	$t1,4
-	mul	$t0,$t0,$t1
-	nop
-	nop
-	add	$t3,$t3,$t0
-	lw	$t1,0($t3)
-	sw	$t1,88($fp)
-	lw	$t0,36($fp)
-	lw	$t1,88($fp)
-	add	$t0,$t0,$t1
-	sw	$t0,84($fp)
-	lw	$2,84($fp)
+	sw	$2,36($fp)
+	lw	$2,36($fp)
 	move	$sp,$fp
-	lw	$fp,96($sp)
-	lw	$31,100($sp)
-	addiu	$sp,$sp,104
+	lw	$fp,44($sp)
+	lw	$31,48($sp)
+	addiu	$sp,$sp,52
 	j	$31
 	nop
 	.end	f
 	.size	f, .-f
+
+	.align	2
+	.globl	f1
+	.ent	f1
+	.type	f1, @function
+f1: 
+	addiu	$sp,$sp,-100
+	sw	$31,96($sp)
+	sw	$fp,92($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$t0,40($fp)
+	li	$t0,0
+	sw	$t0,40($fp)
+	lw	$t3,16($fp)
+	lw	$t0,40($fp)
+	li	$t1,4
+	mul	$t0,$t0,$t1
+	nop
+	nop
+	add	$t3,$t3,$t0
+	sw	$t3,36($fp)
+	lw	$t0,44($fp)
+	li	$t0,4
+	sw	$t0,44($fp)
+	lw	$t0,44($fp)
+	lw	$t1,36($fp)
+	sw	$t0,0($t1)
+	sw	$t0,32($fp)
+	lw	$t0,56($fp)
+	li	$t0,1
+	sw	$t0,56($fp)
+	lw	$t3,16($fp)
+	lw	$t0,56($fp)
+	li	$t1,4
+	mul	$t0,$t0,$t1
+	nop
+	nop
+	add	$t3,$t3,$t0
+	sw	$t3,52($fp)
+	lw	$t0,40($fp)
+	li	$t0,0
+	sw	$t0,40($fp)
+	lw	$t3,16($fp)
+	lw	$t0,40($fp)
+	li	$t1,4
+	mul	$t0,$t0,$t1
+	nop
+	nop
+	add	$t3,$t3,$t0
+	lw	$t1,0($t3)
+	sw	$t1,60($fp)
+	lw	$t0,60($fp)
+	lw	$t1,52($fp)
+	sw	$t0,0($t1)
+	sw	$t0,48($fp)
+	lw	$t0,40($fp)
+	li	$t0,0
+	sw	$t0,40($fp)
+	lw	$t3,16($fp)
+	lw	$t0,40($fp)
+	li	$t1,4
+	mul	$t0,$t0,$t1
+	nop
+	nop
+	add	$t3,$t3,$t0
+	sw	$t3,72($fp)
+	lw	$t0,76($fp)
+	li	$t0,8
+	sw	$t0,76($fp)
+	lw	$t0,76($fp)
+	lw	$t1,72($fp)
+	sw	$t0,0($t1)
+	sw	$t0,68($fp)
+	lw	$t0,68($fp)
+	sw	$t0,28($fp)
+	sw	$t0,64($fp)
+	lw	$t0,40($fp)
+	li	$t0,0
+	sw	$t0,40($fp)
+	lw	$t3,16($fp)
+	lw	$t0,40($fp)
+	li	$t1,4
+	mul	$t0,$t0,$t1
+	nop
+	nop
+	add	$t3,$t3,$t0
+	lw	$t1,0($t3)
+	sw	$t1,84($fp)
+	lw	$t0,28($fp)
+	lw	$t1,84($fp)
+	add	$t0,$t0,$t1
+	sw	$t0,80($fp)
+	lw	$2,80($fp)
+	move	$sp,$fp
+	lw	$fp,92($sp)
+	lw	$31,96($sp)
+	addiu	$sp,$sp,100
+	j	$31
+	nop
+	.end	f1
+	.size	f1, .-f1
 
 .data
 
--- deetz_test_59.c
+++ deetz_test_59.c
@@ -2,7 +2,12 @@ int f1(int a[], int b);
 
 int f(int a, int b)
 {
-	int a[4];
+	int array[4];
+	return f1(array);
+}
+
+int f1(int a[], int b)
+{
 	int x;
 	a[0] = 4;
 	a[1] = a[0];
