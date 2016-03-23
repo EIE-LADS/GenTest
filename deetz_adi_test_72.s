df056527da221d8f4142694f324cb55f0f4cec9f
DiederikVink <dav114@ic.ac.uk>
2016-03-23 05:02:58 +0000

    deetz

--- deetz_adi_test_72.s
+++ deetz_adi_test_72.s
@@ -5,9 +5,9 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-56
-	sw	$31,52($sp)
-	sw	$fp,48($sp)
+	addiu	$sp,$sp,-60
+	sw	$31,56($sp)
+	sw	$fp,52($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
@@ -16,7 +16,9 @@ f:
 	sw	$t0,32($fp)
 	lw	$t0,32($fp)
 	sw	$t0,28($fp)
-	lw	$t0,28($fp)
+	la	$t0,28($fp)
+	sw	$t0,44($fp)
+	lw	$t0,44($fp)
 	move	$a0,$t0
 	sw	$t0,0($fp)
 	la	$t0,f1
@@ -27,9 +29,9 @@ f:
 	sw	$t0,36($fp)
 	lw	$2,28($fp)
 	move	$sp,$fp
-	lw	$fp,48($sp)
-	lw	$31,52($sp)
-	addiu	$sp,$sp,56
+	lw	$fp,52($sp)
+	lw	$31,56($sp)
+	addiu	$sp,$sp,60
 	j	$31
 	nop
 	.end	f
@@ -40,21 +42,24 @@ f:
 	.ent	f1
 	.type	f1, @function
 f1: 
-	addiu	$sp,$sp,-44
-	sw	$31,40($sp)
-	sw	$fp,36($sp)
+	addiu	$sp,$sp,-48
+	sw	$31,44($sp)
+	sw	$fp,40($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
-	lw	$t0,28($fp)
-	li	$t0,3
+	lw	$t0,16($fp)
 	sw	$t0,28($fp)
-	lw	$t0,28($fp)
-	sw	$t0,16($fp)
+	lw	$t0,32($fp)
+	li	$t0,3
+	sw	$t0,32($fp)
+	lw	$t0,32($fp)
+	lw	$t1,28($fp)
+	sw	$t0,0($t1)
 	sw	$t0,24($fp)
 	move	$sp,$fp
-	lw	$fp,36($sp)
-	lw	$31,40($sp)
-	addiu	$sp,$sp,44
+	lw	$fp,40($sp)
+	lw	$31,44($sp)
+	addiu	$sp,$sp,48
 	j	$31
 	nop
 	.end	f1
