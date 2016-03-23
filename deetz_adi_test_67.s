4d8b5391e4677bed61a61e58b5ebabe97e2f6f5f
DiederikVink <dav114@ic.ac.uk>
2016-03-23 04:10:23 +0000

    deetz

---
+++ deetz_adi_test_67.s
@@ -0,0 +1,46 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f: 
+	addiu	$sp,$sp,-76
+	sw	$31,72($sp)
+	sw	$fp,68($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$t0,40($fp)
+	li	$t0,5
+	sw	$t0,40($fp)
+	lw	$t0,40($fp)
+	sw	$t0,36($fp)
+	la	$t0,36($fp)
+	sw	$t0,48($fp)
+	lw	$t0,48($fp)
+	sw	$t0,28($fp)
+	sw	$t0,44($fp)
+	lw	$t0,28($fp)
+	sw	$t0,32($fp)
+	sw	$t0,52($fp)
+	lw	$t0,28($fp)
+	sw	$t0,56($fp)
+	addi	$t0,$t0,4
+	sw	$t0,28($fp)
+	lw	$t0,28($fp)
+	lw	$t1,32($fp)
+	sub	$t0,$t0,$t1
+	sw	$t0,60($fp)
+	lw	$2,60($fp)
+	move	$sp,$fp
+	lw	$fp,68($sp)
+	lw	$31,72($sp)
+	addiu	$sp,$sp,76
+	j	$31
+	nop
+	.end	f
+	.size	f, .-f
+
+.data
+
+
