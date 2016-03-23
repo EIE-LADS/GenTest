f2f852ecf6a2f9e9b6600369fa928c1d7000df7a
DiederikVink <dav114@ic.ac.uk>
2016-03-23 03:45:09 +0000

    deetz

--- deetz_adi_test_66.s
+++ deetz_adi_test_66.s
@@ -4,27 +4,30 @@
 	.ent	f
 	.type	f, @function
 f: 
-	addiu	$sp,$sp,-56
-	sw	$31,52($sp)
-	sw	$fp,48($sp)
+	addiu	$sp,$sp,-64
+	sw	$31,60($sp)
+	sw	$fp,56($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
-	lw	$t0,32($fp)
-	li	$t0,4660
+	lw	$t0,36($fp)
+	li	$t0,5
+	sw	$t0,36($fp)
+	lw	$t0,36($fp)
 	sw	$t0,32($fp)
-	lw	$t0,32($fp)
+	la	$t0,32($fp)
+	sw	$t0,44($fp)
+	lw	$t0,44($fp)
 	sw	$t0,28($fp)
+	sw	$t0,40($fp)
 	lw	$t0,28($fp)
 	lw	$t1,0($t0)
-	sw	$t1,40($fp)
-	lw	$t0,40($fp)
-	sw	$t0,36($fp)
-	lw	$2,36($fp)
+	sw	$t1,48($fp)
+	lw	$2,48($fp)
 	move	$sp,$fp
-	lw	$fp,48($sp)
-	lw	$31,52($sp)
-	addiu	$sp,$sp,56
+	lw	$fp,56($sp)
+	lw	$31,60($sp)
+	addiu	$sp,$sp,64
 	j	$31
 	nop
 	.end	f
