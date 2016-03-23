c25a726719bb11a4be36d1cc5a57ce6ea4b6d23b
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-23 03:10:08 +0000

    arrays working

---
+++ adi_adi_test_66.s
@@ -0,0 +1,37 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f:
+	addiu	$sp,$sp,-64
+	sw	$fp,56($sp)
+	sw	$31,60($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,5
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,28($fp)
+	la	$8,28($fp)
+	sw	$8,44($fp)
+	lw	$8,44($fp)
+	sw	$8,40($fp)
+	sw	$8,36($fp)
+	lw	$8,40($fp)
+	lw	$8,0($8)
+	sw	$8,48($fp)
+	lw	$2,48($fp)
+	move	$sp,$fp
+	lw	$fp,56($sp)
+	lw	$31,60($sp)
+	addiu	$sp,$sp,64
+	j	$31
+	nop
+	.end	f
+	.size	f, .-f
+
+	.data
+
