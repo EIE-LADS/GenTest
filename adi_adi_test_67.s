ea2245d359812faf8c963884d993bcb78b57da3e
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-23 03:12:52 +0000

    pointers

---
+++ adi_adi_test_67.s
@@ -0,0 +1,45 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f:
+	addiu	$sp,$sp,-72
+	sw	$fp,64($sp)
+	sw	$31,68($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,5
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,28($fp)
+	la	$8,28($fp)
+	sw	$8,44($fp)
+	lw	$8,44($fp)
+	sw	$8,40($fp)
+	sw	$8,36($fp)
+	lw	$8,40($fp)
+	sw	$8,48($fp)
+	addi	$8,$8,1
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	addi	$8,$8,1
+	sw	$8,40($fp)
+	sw	$8,56($fp)
+	lw	$8,56($fp)
+	lw	$8,0($8)
+	sw	$8,52($fp)
+	lw	$2,52($fp)
+	move	$sp,$fp
+	lw	$fp,64($sp)
+	lw	$31,68($sp)
+	addiu	$sp,$sp,72
+	j	$31
+	nop
+	.end	f
+	.size	f, .-f
+
+	.data
+
---
+++ adi_test_67.c
@@ -0,0 +1,8 @@
+int f(int a, int b)
+{
+	int x = 5; 
+	int *y; 
+	y = &x;
+	y++; 
+	return *(++y); 
+}
