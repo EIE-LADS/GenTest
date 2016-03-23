36d44d437336a547921f22714eaf166d3bcdf865
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-23 05:02:10 +0000

    pointers partially working

--- adi_adi_test_72.s
+++ adi_adi_test_72.s
@@ -5,9 +5,9 @@
 	.ent	f
 	.type	f, @function
 f:
-	addiu	$sp,$sp,-56
-	sw	$fp,48($sp)
-	sw	$31,52($sp)
+	addiu	$sp,$sp,-60
+	sw	$fp,52($sp)
+	sw	$31,56($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
 	sw	$a1,20($fp)
@@ -16,7 +16,9 @@ f:
 	sw	$8,32($fp)
 	lw	$8,32($fp)
 	sw	$8,28($fp)
-	lw	$8,28($fp)
+	la	$8,28($fp)
+	sw	$8,44($fp)
+	lw	$8,44($fp)
 	move	$a0,$8
 	sw	$8,0($fp)
 	la	$8,f1
@@ -27,9 +29,9 @@ f:
 	sw	$8,36($fp)
 	lw	$2,28($fp)
 	move	$sp,$fp
-	lw	$fp,48($sp)
-	lw	$31,52($sp)
-	addiu	$sp,$sp,56
+	lw	$fp,52($sp)
+	lw	$31,56($sp)
+	addiu	$sp,$sp,60
 	j	$31
 	nop
 	.end	f
@@ -40,21 +42,24 @@ f:
 	.ent	f1
 	.type	f1, @function
 f1:
-	addiu	$sp,$sp,-44
-	sw	$fp,36($sp)
-	sw	$31,40($sp)
+	addiu	$sp,$sp,-48
+	sw	$fp,40($sp)
+	sw	$31,44($sp)
 	move	$fp,$sp
 	sw	$a0,16($fp)
-	lw	$8,28($fp)
-	li	$8,3
+	lw	$8,16($fp)
 	sw	$8,28($fp)
-	lw	$8,28($fp)
-	sw	$8,16($fp)
+	lw	$8,32($fp)
+	li	$8,3
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	lw	$9,28($fp)
+	sw	$8,0($9)
 	sw	$8,24($fp)
 	move	$sp,$fp
-	lw	$fp,36($sp)
-	lw	$31,40($sp)
-	addiu	$sp,$sp,44
+	lw	$fp,40($sp)
+	lw	$31,44($sp)
+	addiu	$sp,$sp,48
 	j	$31
 	nop
 	.end	f1
--- adi_test_72.c
+++ adi_test_72.c
@@ -3,11 +3,11 @@ int f1(int *a);
 int f(int a, int b)
 {
 	int x = 5;
-	int y = f1(x);
+	int y = f1(&x);
 	return x; 
 }
 
 int f1(int *a)
 {
-	a = 3;
+	*a = 3;
 }
