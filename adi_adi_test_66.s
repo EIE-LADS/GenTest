4f91c4a129405239f66daf485299fa89bc87b914
Aditya Rajagopal <Aditya@adityarjagopal3.home>
2016-03-23 03:27:39 +0000

    pointers

---
+++ adi_adi_test_66.s
@@ -0,0 +1,34 @@
+	.text
+	.align	2
+	.globl	f
+	.ent	f
+	.type	f, @function
+f:
+	addiu	$sp,$sp,-56
+	sw	$fp,48($sp)
+	sw	$31,52($sp)
+	move	$fp,$sp
+	sw	$a0,16($fp)
+	sw	$a1,20($fp)
+	lw	$8,32($fp)
+	li	$8,4660
+	sw	$8,32($fp)
+	lw	$8,32($fp)
+	sw	$8,28($fp)
+	lw	$8,28($fp)
+	lw	$8,0($8)
+	sw	$8,40($fp)
+	lw	$8,40($fp)
+	sw	$8,36($fp)
+	lw	$2,36($fp)
+	move	$sp,$fp
+	lw	$fp,48($sp)
+	lw	$31,52($sp)
+	addiu	$sp,$sp,56
+	j	$31
+	nop
+	.end	f
+	.size	f, .-f
+
+	.data
+
